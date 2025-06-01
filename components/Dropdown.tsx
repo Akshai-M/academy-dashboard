6f23d9dd0415342adf1eb295bb2b7cc39d0084a3
akshai-m <mylapuramakshaikumar@gmail.com>
2025-06-01 11:56:25 +0530

    feat: Dropdown page

---
+++ components/Dropdown.tsx
@@ -0,0 +1,40 @@
+"use client"
+
+import * as React from "react"
+
+import { Button } from "../components/ui/button"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuLabel,
+  DropdownMenuRadioGroup,
+  DropdownMenuRadioItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "../components/ui/dropdown-menu"
+import { PanelTopOpen } from "lucide-react"
+
+export default function DropdownMenuRadioGroupDemo() {
+  const [position, setPosition] = React.useState("bottom")
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <div>
+        <label></label>
+        <Button variant="outline">Interview Status <PanelTopOpen /></Button>
+
+        </div>
+        
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="w-56">
+        <DropdownMenuRadioGroup value={position} onValueChange={setPosition}>
+          <DropdownMenuRadioItem value="completed">Completed</DropdownMenuRadioItem>
+          <DropdownMenuRadioItem value="scheduled">Scheduled</DropdownMenuRadioItem>
+          <DropdownMenuRadioItem value="rescheduled">Rescheduled</DropdownMenuRadioItem>
+          <DropdownMenuRadioItem value="pending">Pending</DropdownMenuRadioItem>
+        </DropdownMenuRadioGroup>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}
