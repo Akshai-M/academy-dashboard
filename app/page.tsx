19e777b95897e2578df89a053a30bc2150cea6c5
akshai-m <mylapuramakshaikumar@gmail.com>
2025-09-26 09:46:42 +0530

    perf: refactoring the code for better performance

--- app/page.tsx
+++ app/page.tsx
@@ -4,33 +4,40 @@ import { Phone, LogIn } from "lucide-react";
 import { useRouter } from "next/navigation";
 import axios from "axios";
 import { useStudentStore } from "./store/studentStore";
-import Dashboard from "./components/Dashboard";
+import { useAdminStore } from "./store/adminStore";
 
 export default function LoginForm() {
   const [mobile, setMobile] = useState("");
   const [error, setError] = useState("");
   const [isLoading, setIsLoading] = useState(false);
-  const [isAdmin, setIsAdmin] = useState(false);
   const ADMIN_NUMBER = process.env.NEXT_PUBLIC_ADMIN_NUMBER;
   const router = useRouter();
   const { setStudent } = useStudentStore();
-
+  const { setAdmin } = useAdminStore();
+  const admin = useAdminStore((state) => state.admin);
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
     setError("");
     setIsLoading(true);
 
     try {
+
       if (mobile.length == 36 && mobile === ADMIN_NUMBER) {
-        setIsAdmin(true);
+       
+        const res = await axios.get("/api/students");
+        setAdmin(res.data[0])
+        console.table(admin);
+        router.push("/dashboard");
         return;
       }
 
+      
       const res = await axios.post("/api/students", { mobile });
 
       if (res.status === 200) {
         setStudent(res.data);
-        router.push("/students");
+        console.table(res.data);
+        router.push("/report");
       }
     } catch (err: any) {
       if (err.response?.status === 404) {
@@ -42,12 +49,9 @@ export default function LoginForm() {
       setIsLoading(false);
     }
   };
-  if (isAdmin) {
-    return <Dashboard />;
-  }
 
   return (
-    <div className="min-h-screen bg-gradient-to-br from-blue-50 to-indigo-100 flex items-center justify-center p-4">
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center p-4">
       <div className="bg-white rounded-2xl shadow-xl p-8 w-full max-w-md">
         <div className="text-center mb-8">
           <div className="bg-blue-100 w-16 h-16 rounded-full flex items-center justify-center mx-auto mb-4">
