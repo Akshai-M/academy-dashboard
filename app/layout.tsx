91e3ebe9622cc0e35805bc6802a19f95c082f402
akshai-m <mylapuramakshaikumar@gmail.com>
2025-09-27 12:28:42 +0530

    refactor: Fixing the allignment of the code

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,24 @@
-import React from "react";
-import './globals.css';
+import BackendEditModal from "./components/BackendEditModal";
+import CandidateEditModal from "./components/CandidateEditModal";
+import FrontendEditModal from "./components/FrontendEditModal";
+import { ModalProvider } from "./modalContext";
+import "./globals.css";
 
-export default function RootLayout({ children}: {children: React.ReactNode}) {
-    return (
-        <html lang="en">
-            <body>
-                {children}
-            </body>
-        </html>
-    )
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>
+        <ModalProvider>
+          {children}
+          <CandidateEditModal />
+          <FrontendEditModal />
+          <BackendEditModal />
+        </ModalProvider>
+      </body>
+    </html>
+  );
 }
