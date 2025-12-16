c504a428f98cf824e474e8e639c400fe8b662ecd
akshai-m <mylapuramakshaikumar@gmail.com>
2025-12-16 18:23:24 +0530

    refactor: adding the decrypt to the route

--- app/api/students/route.ts
+++ app/api/students/route.ts
@@ -1,7 +1,13 @@
 import { PrismaClient } from "@prisma/client";
 import { NextRequest, NextResponse } from "next/server";
+import crypto from "crypto";
 
 const prisma = new PrismaClient();
+
+function hashMobile(text: string) {
+  return crypto.createHash("sha256").update(text).digest("hex");
+}
+
 
 export async function POST(request: NextRequest) {
   try {
@@ -25,8 +31,10 @@ export async function POST(request: NextRequest) {
       return res;
     }
 
+    const mobileHash = hashMobile(mobile);
+
     const student = await prisma.candidate_interviews.findFirst({
-      where: { mobile_number: mobile },
+      where: { mobile_hash: mobileHash },
     });
 
     if (!student) {
@@ -36,25 +44,59 @@ export async function POST(request: NextRequest) {
       );
     }
 
-    const res = NextResponse.json(student, { status: 200 });
+    const decryptedStudent = {
+      ...student,
+      mobile_number: decrypt(student.mobile_number),
+    };
+
+    const res = NextResponse.json(decryptedStudent, { status: 200 });
+
     res.cookies.set("session", mobile, {
       httpOnly: true,
-      secure: false, // change to true in production
+      secure: false,
       sameSite: "strict",
       path: "/",
     });
+
     return res;
   } catch (error) {
-    console.error(error);
+    console.error("Login Error:", error);
     return NextResponse.json({ message: "Server error" }, { status: 500 });
   }
 }
 
+const SECRET_KEY = process.env.PHONE_SECRET_KEY as string;
+
+function getValidKey(key: string) {
+  return crypto.createHash("sha256").update(key).digest();
+}
+
+function decrypt(encryptedText: string) {
+  const [ivHex, encrypted] = encryptedText.split(":");
+
+  const iv = Buffer.from(ivHex, "hex");
+  const key = getValidKey(SECRET_KEY);
+
+  const decipher = crypto.createDecipheriv("aes-256-cbc", key, iv);
+
+  let decrypted = decipher.update(encrypted, "hex", "utf8");
+  decrypted += decipher.final("utf8");
+
+  return decrypted;
+}
+
 export async function GET() {
   try {
-    const adminData = await prisma.candidate_interviews.findMany()
-    return NextResponse.json(adminData, { status: 200 })
+    const adminData = await prisma.candidate_interviews.findMany();
+
+    const decryptedData = adminData.map(item => ({
+      ...item,
+      mobile_number: decrypt(item.mobile_number),
+    }));
+
+    return NextResponse.json(decryptedData, { status: 200 });
   } catch (error) {
-    return NextResponse.json({ message: `Server error ${error}` }, { status: 500 })
+    return NextResponse.json({ message: `Server error ${error}` }, { status: 500 });
   }
 }
+
