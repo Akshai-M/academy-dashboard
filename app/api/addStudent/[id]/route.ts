9763f350c4e44a92be612b7e5e93d8229d35ecae
akshai-m <mylapuramakshaikumar@gmail.com>
2025-12-16 18:00:12 +0530

    refactor: encrypting the numbers

--- app/api/addStudent/[id]/route.ts
+++ app/api/addStudent/[id]/route.ts
@@ -1,21 +1,3 @@
-// import { NextResponse } from "next/server";
-// import { PrismaClient } from "@prisma/client";
-
-// const prisma = new PrismaClient();
-
-// export async function PATCH(req: Request, context: { params: Promise<{ id: string }> }) {
-//   const {id} = await context.params;
-//   const data = await req.json();
-  
-
-//   const updated = await prisma.candidate_interviews.update({
-//     where: { user_id: id },
-//     data,
-//   });
-
-//   return NextResponse.json(updated);
-// }
-
 import { NextResponse } from "next/server";
 import { PrismaClient } from "@prisma/client";
 
@@ -35,6 +17,22 @@ export async function PATCH(
   if (!existing) {
     return NextResponse.json({ error: "Candidate not found" }, { status: 404 });
   }
+
+  function encrypt(text: string) {
+    const crypto = require("crypto");
+    const iv = crypto.randomBytes(16);
+    const SECRET_KEY = process.env.PHONE_SECRET_KEY as string;
+    const key = crypto.createHash("sha256").update(SECRET_KEY).digest();
+
+    const cipher = crypto.createCipheriv("aes-256-cbc", key, iv);
+    let encrypted = cipher.update(text, "utf8", "hex");
+    encrypted += cipher.final("hex");
+
+    return `${iv.toString("hex")}:${encrypted}`;
+  }
+  if (data.mobile_number) {
+    data.mobile_number = encrypt(data.mobile_number);
+  } 
 
   if (data.strongest_skill) {
     const incomingSkills = data.strongest_skill
